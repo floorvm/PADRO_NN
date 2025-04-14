0402d1f8881edb2e369f6fb0f9ca04bad77d6b11
floorvm <floorvm@live.com>
2025-04-14 15:32:57 +0200

    matching shapes

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ transform = transforms.Compose([transforms.Resize(imgSize), transforms.ToTensor(
 trainDataset = MNISTBlurredDataset(H, path, train=True, download=True, transform=transform)
 trainLoader = DataLoader(trainDataset, batch_size=batchSize, shuffle=True, drop_last=True)
 trainLoaderSize = len(trainLoader)
-# plot_images(trainLoader, num_images=5)
+plot_images(trainLoader, num_images=2)
 
 testDataset = MNISTBlurredDataset(H, path, train=False, download=True, transform=transform)
 testLoader = DataLoader(testDataset, batch_size=batchSize, shuffle=True, drop_last=True)
